446a74c6ab62a00138824627d3c54a59436b060d
Karina Proenca <karina.kirisitina@gmail.com>
2025-08-11 11:41:52 -0400

    Projeto

--- Tela-interativa/script.js
+++
@@ -1,9 +0,0 @@
-  let card = document.querySelector(".card");
-        let loginButton = document.querySelector(".loginButton");
-        let cadastroButton = document.querySelector9(".cadasatroButton");
-
-        loginButton.onclick = () => {
-            card.classList.remove("cadastroActive")
-            card.classList.add("loginActive")
-            
-        }
